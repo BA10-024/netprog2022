9add4cbd7f128cbef12c5f949e47d60827448923
BA10-024 <huyenkk.ba10-024@st.usth.edu.vn.com>
2022-05-30 23:11:20 -0700

    Practical work 3

---
+++ 03.practical.work.server.setup.c
@@ -0,0 +1,35 @@
+i#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netdb.h>
+#include <arpa/inet.h>   
+int main () {
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    unsigned short port = 8784;
+	
+    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error creating socket.");
+    }
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+    if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+	    printf("Error binding.");
+    }
+    if (listen(sockfd, 5) < 0) {
+        printf("Error listening.");
+
+    }
+    clen=sizeof(caddr);
+
+    if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) {
+        printf("Error connection.");
+
+    }  
+    printf("Successful connected.");
+    return 0;
+ }
